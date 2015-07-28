47e30bd1c4ebd7fb498c522f6be4c169f6594270
Daniel Rasmuson <dan123911@gmail.com>
2015-07-28 15:35:11 -0500

    add a performance test

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-You can find the full article here...
+This is just a repo for me to mess around with web workers. 
+
+I've done some performance metrics if you were running a computation intensive task.
+
+Here is a good article to start with if you want to do the same...
 http://www.html5rocks.com/en/tutorials/workers/basics/
--- main.js
+++ main.js
@@ -1,7 +1,43 @@
-var worker = new Worker('doWork.js');
+function spawnLargeTask(callback){
+  var deferred = Q.defer();
 
-worker.addEventListener('message', function(e) {
-  console.log('Worker said: ', e.data);
-}, false);
+  var worker = new Worker('largeTask.js');
+  worker.addEventListener('message', function(e) {
+    console.log('Large Task Result: ', e.data);
+    deferred.resolve(e.data);
+  }, false);
+  worker.postMessage({}); // Send data to our worker.
 
-worker.postMessage('Hello World'); // Send data to our worker.
+  return deferred.promise;
+}
+
+// Start timing now
+console.time("concatenation");
+
+var demons = [spawnLargeTask(),
+  spawnLargeTask(),
+  spawnLargeTask(),
+  spawnLargeTask(),
+  spawnLargeTask(),
+  spawnLargeTask(),
+  spawnLargeTask()
+]
+
+// ... and stop.
+console.timeEnd("concatenation");
+
+
+// function loopLargeRange(){
+//   function getRandomInt(min, max) {
+//       return Math.floor(Math.random() * (max - min + 1)) + min;
+//   }
+
+//   var x = 0;
+//   var fiveMillion = Math.pow(10, 6)*5;
+//   var tenMillion = Math.pow(10, 7);
+//   var limit = getRandomInt(fiveMillion, tenMillion);
+//   while (x < limit){
+//     x++
+//   }
+//   return x;
+// }
