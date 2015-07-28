5c94ce047f581977da4d08d1f3a72c704a996643
Daniel Rasmuson <dan123911@gmail.com>
2015-07-28 16:11:34 -0500

    added an inline example from html5 and upped the looping range for effect

--- largeTask.js
+++ largeTask.js
@@ -1,17 +1,7 @@
-/**
-* Returns a random integer between min (inclusive) and max (inclusive)
-* Using Math.round() will give you a non-uniform distribution!
-*/
 function loopLargeRange(){
-  function getRandomInt(min, max) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
   var x = 0;
-  var fiveMillion = Math.pow(10, 6)*5;
-  var tenMillion = Math.pow(10, 7);
-  var limit = getRandomInt(fiveMillion, tenMillion);
-  while (x < limit){
+  var fiveHundredMillion = Math.pow(10, 8)*5;
+  while (x < fiveHundredMillion){
     x++
   }
   return x;
--- main.js
+++ main.js
@@ -1,6 +1,18 @@
+// this is the function I'm testing the performance on
+// its defined in both this file and largeTask
+function loopLargeRange(){
+  var x = 0;
+  var fiveHundredMillion = Math.pow(10, 8)*5;
+  while (x < fiveHundredMillion){
+    x++
+  }
+  return x;
+}
+
 function spawnLargeTask(callback){
   var deferred = Q.defer();
 
+  // this is requesting the largeTask.js 10x 
   var worker = new Worker('largeTask.js');
   worker.addEventListener('message', function(e) {
     deferred.resolve(e.data);
@@ -14,5 +26,12 @@ console.time("Web Worker Total Time");
 Q.all(_.times(10, spawnLargeTask))
   .then(function(results){
     console.timeEnd("Web Worker Total Time");
+  })
+  .then(function(){
+    // I'm guessing the performance isn't up
+    // because the http request are taking longer then the actual computation
+    console.time("Looping 10 Ranges");
+    _.times(10, loopLargeRange)
+    console.timeEnd("Looping 10 Ranges");
   })
 
